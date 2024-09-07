dd776748adf38b511e46de64ece952cdc7a70625
SalmaJr <48619206+SalmaJr@users.noreply.github.com>
2024-09-07 15:50:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/consultants', (req, res) => {
+  const consultants = [
+    { id: 1, firstName: 'Ahmed', lastName: 'El Haddad', CIN: 'A123456' },
+    { id: 2, firstName: 'Fatima', lastName: 'Ben Ali', CIN: 'B654321' }
+  ];
+  res.json(consultants);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
