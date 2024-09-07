911d4421fd4a166679bee63d563684caaa5d4e3a
RIBIEMC2 <ribi.youness.emc2@ehtp.ac.ma>
2024-09-07 17:03:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const app = express();
+const express = require('express'); 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const app = express(); 
 
-const port = process.env.PORT || 3000;
+ 
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.get('/consultants', (req, res) => { 
 
+  const consultants = [ 
+
+    { id: 1, firstName: 'Ahmed', lastName: 'El Haddad', CIN: 'A123456' }, 
+
+    { id: 2, firstName: 'Fatima', lastName: 'Ben Ali', CIN: 'B654321' } 
+
+  ]; 
+
+  res.json(consultants); 
+
+}); 
+
+ 
+
+app.listen(process.env.PORT || 3000, () => { 
+
+  console.log('Server running'); 
+
+}); 
