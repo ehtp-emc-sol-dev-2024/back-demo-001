51d0e0f3bf215a69dd3a8cc9dbc238b3b141ac3a
Fatimazahra10 <101047121+Fatimazahra10@users.noreply.github.com>
2024-09-08 15:13:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.get('/consultants', (req, res) => {
 
 
 app.get('/consultants/:id', async (req, res) => {
-  res.json(consultants);
+    consultant =    { "id" : 3, "firstName" : "Sam", "lastName" : "Green", "CIN" : "C987654", "skills" : [ "AWS", "SQL", "MongoDB", "Docker" ] } ;
+   res.json(consultant);
 });
 
 app.listen(process.env.PORT || 3000, () => {
