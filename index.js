43a367daae7af992b264b7e69e1967b6a461f0f5
hatimlechgar <h.lechgar@gmail.com>
2024-09-08 15:54:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.get('/consultants', (req, res) => {
-  const consultants = [
-    { id: 1, firstName: 'Ahmed', lastName: 'El Haddad', CIN: 'A123456' },
-    { id: 2, firstName: 'Fatima', lastName: 'Ben Ali', CIN: 'B654321' }
+const consultants = [
+    { "id" : 1, "firstName" : "John", "lastName" : "Doe", "CIN" : "A123456", "skills" : [ "Java", "Python", "Azure" ] },
+    { "id" : 2, "firstName" : "Jane", "lastName" : "Smith", "CIN" : "B654321", "skills" : [ "JavaScript", "React", "Node.js" ] },
+    { "id" : 3, "firstName" : "Sam", "lastName" : "Green", "CIN" : "C987654", "skills" : [ "SQL", "MongoDB", "Docker" ] }
   ];
+
+app.get('/consultants', (req, res) => {
+
+  res.json(consultants);
+});
+
+
+app.get('/consultants/:id', async (req, res) => {
   res.json(consultants);
 });
 
