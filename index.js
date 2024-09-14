ca949e0e1c102656765a55fd9d00878686294688
RIBIEMC2 <ribi.youness.emc2@ehtp.ac.ma>
2024-09-14 14:36:26 +0100

    Update index.js 2

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ app.get('/consultants', (req, res) => {
 
 }); 
 
+app.get('/consultants/:id', async (req, res) => {
+    consultant =    { "id" : 3, "firstName" : "Sam", "lastName" : "Green", "CIN" : "C987654", "skills" : [ "AWS", "SQL", "MongoDB", "Docker" ] } ;
+   res.json(consultant);
+});
+
  
 
 app.listen(process.env.PORT || 3000, () => { 
